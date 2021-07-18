634e7306b870c1e0a7fd31820c80073e7c8ec9d8
Glenn Watson <GitHub@glennwatson.net>
2021-07-18 15:37:52 +1000

    Fix

--- src/Splat.DependencyInjection.SourceGenerator.Tests/RegisterTests.LazyParameterNotRegisteredLazyFail_contract=Test1.02.verified.cs
+++ src/Splat.DependencyInjection.SourceGenerator.Tests/RegisterTests.LazyParameterNotRegisteredLazyFail_contract=Test1.02.verified.cs
@@ -5,7 +5,7 @@ namespace Splat
 {
     internal static partial class SplatRegistrations
     {
-        static partial void SetupIOCInternal()
+        static partial void SetupIOCInternal( Splat.IDependencyResolver resolver) 
         {
             Splat.Locator.CurrentMutable.Register(() => new global::Test.Service1(), typeof(global::Test.IService1), "Test1");
             Splat.Locator.CurrentMutable.Register(() => new global::Test.Service2(), typeof(global::Test.IService2), "Test1");
